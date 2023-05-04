7ca935f57dc8f42765e47181f8380c557d5a49c7
sungwoong-Noe <asdz453@gmail.com>
2023-05-05 00:12:26 +0900

    front단 - 자식카테고리 등록 및 출력

--- src/main/resources/static/js/category/category.js
+++ src/main/resources/static/js/category/category.js
@@ -1,5 +1,37 @@
 'use strict';
 
+
+const categoryRegistRequest = (parentId, categoryName) => {
+    const formData = {};
+    formData['categoryName'] = categoryName;
+    if(parentId) formData['parentId'] = parentId;
+
+    let response = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/category", false);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+
+
+            response = xhr.responseText;
+
+        }else {
+            console.log('error', xhr.responseText);
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+
+
+    if (response !== null) {
+        return response;
+    }else {
+        return null;
+    }
+}
+
 const parentCategoryRegist = () => {
 
     const parent_name = document.querySelector('#parent_category_name').value;
@@ -12,60 +44,80 @@ const parentCategoryRegist = () => {
         const formData = {};
         formData['categoryName'] = parent_name;
 
-        const xhr = new XMLHttpRequest();
 
-        xhr.open("POST", "/category", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
+        let parentResponse = categoryRegistRequest(null, parent_name);
 
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        if (!parentResponse) {
+            alert("등록에 실패하였습니다.");
+            return false;
+        } else {
+            parentResponse = JSON.parse(parentResponse);
+        }
 
-                const categoryList = document.querySelector('#accordionExample');
-                const parentResponse = JSON.parse(xhr.responseText);
+        console.log(parentResponse);
+        const categoryList = document.querySelector('#accordionExample');
 
-                const $parentCategory = `<div class="accordion-item">
-                                          <h2 class="accordion-header" id="heading${parentResponse.id}">
-                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${parentResponse.id}" aria-expanded="true" aria-controls="collapse${parentResponse.id}">
-                                              ${parentResponse.categoryName}
-                                            </button>
-                                          </h2>
-                                          <div id="collapse${parentResponse.id}" class="accordion-collapse collapse" aria-labelledby="heading${parentResponse.id}" data-bs-parent="#accordionExample">
-                                            <div class="accordion-body">
-                                
-                                              <table class="table">
-                                                <thead>
-                                                <tr>
-                                                  <th scope="col">카테고리명</th>
-                                                  <th scope="col"></th>
-                                                </tr>
-                                                </thead>
-                                                <tbody>
-                                
-                                                <tr>
-                                                  <td>
-                                                    <div class="input-group mb-3">
-                                                      <input type="text" class="form-control" placeholder="카테고리명" aria-label="Recipient's username" aria-describedby="button-addon2">
-                                                      <button class="btn btn-outline-secondary" type="button" id="button-addon2">+</button>
-                                                    </div>
-                                                  </td>
-                                                </tr>
-                                                </tbody>
-                                              </table>
+        const $parentCategory = `<div class="accordion-item">
+                                  <h2 class="accordion-header" id="heading${parentResponse.id}">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${parentResponse.id}" aria-expanded="true" aria-controls="collapse${parentResponse.id}">
+                                      ${parentResponse.categoryName}
+                                    </button>
+                                  </h2>
+                                  <div id="collapse${parentResponse.id}" class="accordion-collapse collapse" aria-labelledby="heading${parentResponse.id}" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                        
+                                      <table class="table">
+                                        <thead>
+                                        <tr>
+                                          <th scope="col">카테고리명</th>
+                                          <th scope="col"></th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                        
+                                        <tr>
+                                          <td>
+                                            <div class="input-group mb-3">
+                                              <input type="text" class="form-control" placeholder="카테고리명" aria-label="Recipient's username" aria-describedby="btn_childCategoryRegist${parentResponse.id}">
+                                              <button class="btn btn-outline-secondary" type="button" id="btn_childCategoryRegist${parentResponse.id}" onclick="childCategoryRegist(${parentResponse.id}, this)">+</button>
                                             </div>
-                                          </div>
-                                        </div>`;
+                                          </td>
+                                        </tr>
+                                        </tbody>
+                                      </table>
+                                    </div>
+                                  </div>
+                                </div>`;
 
-                categoryList.insertAdjacentHTML('beforeend', $parentCategory);
-                document.querySelector('#parent_category_name').value = ``;
+        categoryList.insertAdjacentHTML('beforeend', $parentCategory);
+        document.querySelector('#parent_category_name').value = ``;
 
-            } else {
-                console.log('response', xhr.responseText);
-            }
-        };
-
-        xhr.send(JSON.stringify(formData));
     }
 }
 
 
 document.querySelector('#btn_parent_regist').addEventListener('click', parentCategoryRegist);
+
+
+const childCategoryRegist = (parentId, clickButtonTag) => {
+
+    const childCategoryName = clickButtonTag.previousSibling.previousSibling.value;
+
+    let childCategoryResponse = categoryRegistRequest(parentId, childCategoryName);
+
+    if (childCategoryResponse !== null) {
+        childCategoryResponse = JSON.parse(childCategoryResponse);
+        const $childCategoryElement = ` <tr>
+                                          <td>${childCategoryResponse.categoryName}</td>
+                                          <td>
+                                            <button type="button" class="btn btn-danger btn-sm">삭제</button>
+                                          </td>
+                                        </tr>`;
+        const $registButtonTr = clickButtonTag.parentElement.parentElement.parentElement;
+        $registButtonTr.insertAdjacentHTML('beforebegin', $childCategoryElement);
+        clickButtonTag.previousSibling.previousSibling.value = ``;
+    }
+
+
+    console.log(childCategoryResponse);
+}
