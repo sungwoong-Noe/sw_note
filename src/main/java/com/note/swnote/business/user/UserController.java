0fa2a9c4d01af7551d7e47c2f66e5c3390e14901
sungwoong-Noe <asdz453@gmail.com>
2023-07-23 14:09:55 +0900

    security commit

---
+++ src/main/java/com/note/swnote/business/user/UserController.java
@@ -0,0 +1,12 @@
+package com.note.swnote.business.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+}
