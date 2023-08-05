ab5b89764dcd1710ef8209f8a788a913f3dfd13f
sungwoong-Noe <asdz453@gmail.com>
2023-08-05 14:52:11 +0900

    properties 설정

--- src/main/java/com/note/swnote/SwnoteApplication.java
+++ src/main/java/com/note/swnote/SwnoteApplication.java
@@ -2,8 +2,10 @@ package com.note.swnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/s3.properties")
 public class SwnoteApplication {
 
     public static void main(String[] args) {
